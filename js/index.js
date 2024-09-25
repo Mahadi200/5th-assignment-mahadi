577646a5b19a2a070ed5c09b280cdb0fde943a45
Mahadi200 <shurov895@gmail.com>
2024-09-24 22:01:54 -0700

    added js for frist card

--- js/index.js
+++ js/index.js
@@ -5,12 +5,19 @@ document.getElementById('donate-btn')
     const addMoney = document.getElementById('input-donate').value;
     const addMoneyNumber = parseFloat(addMoney);
     
-    
-    if (addMoney !== '-') {
+
+    if (addMoney !== isNaN ) {
       const balance = document.getElementById('amount-is').innerText;
-    const balanceNumber = parseFloat(balance);
+      const balanceNumber = parseFloat(balance);
       const NewBalance = addMoneyNumber + balanceNumber;
-     
+
+      const mainBalanec = document.getElementById('is-money').innerText;
+      console.log(mainBalanec);
+
+      const mainBalanceNumber = parseFloat(mainBalanec);
+      const addmainbalanec = mainBalanceNumber - addMoneyNumber;
+      document.getElementById('is-money').innerText = addmainbalanec;
+
       document.getElementById('amount-is').innerText = NewBalance;
 
     } else {
